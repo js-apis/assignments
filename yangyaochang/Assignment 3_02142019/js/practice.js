a05e878297ab7b58aa3ac24a827bfb5523e73b5f
yangyaochang <42128044+yangyaochang@users.noreply.github.com>
2019-02-28 13:15:29 -0500

    Add files via upload

---
+++ yangyaochang/Assignment 3_02142019/js/practice.js
@@ -0,0 +1,33 @@
+var baseURL = 'https://api.airvisual.com/v2/countries?key=';
+var apiKey = 'BP3iQAPQiYht8cWpR';
+var endPointURL = baseURL + apiKey;
+var result;
+
+function getJSON(path) {
+  var req = new XMLHttpRequest();  
+  req.open('GET', path, true);
+  req.responseType = 'json';
+  req.onload = function() {
+    result = req.response;
+  };
+  req.send();
+}
+
+// function render() {
+//   var containerEl = document.querySelector('#container');
+  
+//   for(var i = 0; i < result.data.length; i++) {
+//     var placeEl = document.createElement('div');
+//     placeEl.classList.add('place');
+
+//     var countryEl = document.createElement('h3');
+//     countryEl.innerText = result.data[i];
+//     countryEl.classList.add('country');
+
+//     placeEl.append(countryEl);
+//     containerEl.append(placeEl);
+//   }
+// }
+
+getJSON(endPointURL);
+// render();
